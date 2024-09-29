1e9fa26870689cdb864cb547e3df75a90c248f86
WaqarAhamadWD <wa8287683@gmail.com>
2024-09-29 22:13:32 +0500

    fixed issue

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
-import Form from "./Form.vue";
+import FormD from "./FormD.vue";
 // import DynamicListModel from "./dynamic_list_model.vue";
 // import DynamicList from "./dynamic_list.vue";
 // import ImageComponent from "./image.vue";
 // import sidebar from "./sidebar.vue";
-export { Form };
+export { FormD };
